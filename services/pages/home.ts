638a80e26f4384af90ad768ea7a0f78abc148729
vasilkovalyov <vasilkovalyov@gmail.com>
2023-10-07 00:44:44 +0300

    removed useless data for page response

--- services/pages/home.ts
+++ services/pages/home.ts
@@ -1,15 +1,12 @@
-import { BlockHeroModel, IBlockAboutCourse, IBlockHero } from "../../models";
+import { BlockHeroModel, IBlockHero } from "../../models";
 import { IPage } from "./type";
 
 class PageHomeService {
   async getPage(): Promise<IPage> {
     const page = "home";
     const post = await BlockHeroModel.findOne({ block_page: page, publish: true });
+
     return {
-      seo: {
-        title: "Home page",
-        description: "Home page description",
-      },
       body: [post as IBlockHero],
     };
   }
