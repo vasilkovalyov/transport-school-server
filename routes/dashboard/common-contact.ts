f17c2ef6a2b65b84dd64003878b16ed3f6419634
vasilkovalyov <vasilkovalyov@gmail.com>
2023-10-10 00:44:48 +0300

    added api for social get info

--- routes/dashboard/common-contact.ts
+++ routes/dashboard/common-contact.ts
@@ -5,8 +5,9 @@ import authMiddleware from "../../middlewares/auth";
 const router = express.Router();
 const controller = new CommonContactsController();
 
-router.post("/contacts", (res, req) => controller.updateContacts(res, req));
-router.post("/socials", (res, req) => controller.updateSocial(res, req));
+router.patch("/contacts", (res, req) => controller.updateContacts(res, req));
+router.patch("/socials", (res, req) => controller.updateSocial(res, req));
 router.get("/contacts", (res, req) => controller.getContacts(res, req));
+router.get("/socials", (res, req) => controller.getSocials(res, req));
 
 export default router;
