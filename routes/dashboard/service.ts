e88e2fe818c8497713d7dd25f1c60168c4090095
vasilkovalyov <vasilkovalyov@gmail.com>
2023-10-30 11:19:03 +0200

    fixed service update route

--- routes/dashboard/service.ts
+++ routes/dashboard/service.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 const serviceController = new ServiceController();
 
 router.post("/service-create", (req, res) => serviceController.create(req, res));
-router.patch("/service-update/:id", (req, res) => serviceController.update(req, res));
+router.patch("/service-update", (req, res) => serviceController.update(req, res));
 router.delete("/service-delete/:id", (req, res) => serviceController.delete(req, res));
 router.get("/services", (req, res) => serviceController.getServices(req, res));
 router.get("/services/:id", (req, res) => serviceController.getService(req, res));
