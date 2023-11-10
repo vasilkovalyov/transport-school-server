51db429f6e17ae3e2cca1ba6aee2cea63a35ebf0
vasilkovalyov <vasilkovalyov@gmail.com>
2023-11-10 21:10:52 +0200

    removed auth check from public blog api

--- routes/public/posts.ts
+++ routes/public/posts.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { PostsController } from "../../controllers/public";
-import authMiddleware from "../../middlewares/auth";
 
 const router = express.Router();
 const controller = new PostsController();
 
-router.get("/posts", authMiddleware, (res, req) => controller.getPaginatedPosts(res, req));
-router.get("/posts/:slug", authMiddleware, (res, req) => controller.getPostBySlug(res, req));
+router.get("/posts", (res, req) => controller.getPaginatedPosts(res, req));
+router.get("/posts/:slug", (res, req) => controller.getPostBySlug(res, req));
 
 export default router;
